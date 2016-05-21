10d906f59e18b001533bcfd95a73f61addd2705e
Alan Inacio <Alan Inacio>
2016-05-21 15:45:29 -0400

    adding angular material

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular
-    .module('app', ['ngRoute', 'firebase'])
+    .module('app', ['ngRoute', 'firebase', 'ngMaterial'])
     .config(mainConfig);
 
 mainConfig.$inject = ['$routeProvider', '$firebaseRefProvider'];
