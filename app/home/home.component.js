23a5d0f1fc49846683b8ba09520b4c0cc3721e88
Alan Inacio <Alan Inacio>
2016-05-21 16:32:02 -0400

    join added to home

--- app/home/home.component.js
+++ app/home/home.component.js
@@ -1,3 +1,19 @@
 angular.module('app').component('home', {
-   templateUrl: '/home/home.html'
+   templateUrl: '/home/home.html',
+   controller: welcomeController
 });
+
+welcomeController.$inject = ['$firebaseAuthService', '$location'];
+
+function welcomeController($firebaseAuthService, $location) {
+    this.annonLogin = function() {
+           $firebaseAuthService.$authAnonymously()
+               .then((function() {
+                   console.log('logged in anonymously', this.sessionId);
+               }).bind(this))
+               .catch(function() {
+                   console.log('nope');
+               })
+           
+       }
+}
