7588a116234caed6ad867ab862feed1ee814e48e
Alan Inacio <Alan Inacio>
2016-05-21 15:12:02 -0400

    anonymous auth clalback

--- app/join/join.component.js
+++ app/join/join.component.js
@@ -10,9 +10,9 @@ angular
    function joinController($firebaseAuthService, $location) {
        this.annonLogin = function() {
            $firebaseAuthService.$authAnonymously()
-               .then(function() {
-                   console.log('logged in anonymously');
-               })
+               .then((function() {
+                   console.log('logged in anonymously', this.sessionId);
+               }).bind(this))
                .catch(function() {
                    console.log('nope');
                })
